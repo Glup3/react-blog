c2854628a536652fc1bec9ec160af7ddaa7c3fc9
Glup3 <glupstar312@gmail.com>
2018-12-27 10:12:48 +0100

    add dashboard skeleton

---
+++ src/components/blog/BlogPostList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BlogPostList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card-z-depth-0 blogpost-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Title</span>
+          <p>Text</p>
+          <p className="grey-text">Date</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BlogPostList
