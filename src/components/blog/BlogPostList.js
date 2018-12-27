77fa26e265fa7aace99b91b9f0030ab26484e817
Glup3 <glupstar312@gmail.com>
2018-12-27 10:16:41 +0100

    add generic blogpost summary

--- src/components/blog/BlogPostList.js
+++ src/components/blog/BlogPostList.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import BlogPostSummary from './BlogPostSummary';
 
 const BlogPostList = () => {
   return (
     <div className="project-list section">
-      <div className="card-z-depth-0 blogpost-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Title</span>
-          <p>Text</p>
-          <p className="grey-text">Date</p>
-        </div>
-      </div>
+      <BlogPostSummary />
+      <BlogPostSummary />
+      <BlogPostSummary />
     </div>
   )
 }
---
+++ src/components/blog/BlogPostSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BlogPostSummary = () => {
+  return (
+    <div className="card-z-depth-0 blogpost-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Title</span>
+        <p>Text</p>
+        <p className="grey-text">Date</p>
+      </div>
+    </div>
+  )
+}
+
+export default BlogPostSummary
