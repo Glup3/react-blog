6ade6ca0d0dad1c74eb926c5abf31bd19ab7322f
Glup3 <glupstar312@gmail.com>
2018-12-27 10:49:06 +0100

    add sign up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import BlogPostDetails from './components/blog/BlogPostDetails';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={Dashboard} />
             <Route path="/blog/:id" component={BlogPostDetails} />
             <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
