1aa9db4c8f6cc1c48fc3c3acc501db1cd86a3262
Glup3 <glupstar312@gmail.com>
2018-12-27 10:37:35 +0100

    add blogpost details skeleton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import BlogPostDetails from './components/blog/BlogPostDetails';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/blog/:id" component={BlogPostDetails} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/components/blog/BlogPostDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const BlogPostDetails = () => {
+  return (
+    <div>
+      <div className="container section blogpost-details">
+        <div className="card z-depth-0">
+          <span className="card-title">Title</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sequi ex tempore illo, veritatis, iure sapiente eius soluta quis voluptatem quaerat nesciunt aperiam in perferendis qui numquam quod maxime quibusdam.</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by Glup3</div>
+          <div>Date</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BlogPostDetails
