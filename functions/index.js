737730275b10ff7248f20e663dff6a196ac67956
Glup3 <glupstar312@gmail.com>
2018-12-28 14:57:29 +0100

    add notification cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+
+const createNotification = (notification) => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then((doc) => {
+      console.log('Notification added', doc);
+    })
+}
+
+exports.blogpostCreated = functions.firestore
+  .document('blogposts/{blogpostId}')
+  .onCreate(doc => {
+    const blogpost = doc.data();
+    const notification = {
+      content: 'Added a new blogpost',
+      user: `${blogpost.authorFirstName} ${blogpost.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    return createNotification(notification);
+  })
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
