2d05a9b894f9989c79a8b122b0cdb54a489160ce
Glup3 <glupstar312@gmail.com>
2018-12-28 19:25:59 +0100

    beautify notifications page

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -7,15 +7,15 @@ const Notifications = (props) => {
   return (
     <div className="section">
       <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Notifications</span>
+        <div className="card-content notification-container">
+          <span className="card-title black-text">Notifications</span>
           <ul className="notifications">
             { notifications && notifications.map((notification) => {
               return (
                 <li key={ notification.id }>
-                  <span className="pink-text">{ notification.user } </span>
+                  <span className="purple-text text-accent-3">{ notification.user } </span>
                   <span>{ notification.content }</span>
-                  <div className="grey-text note-date">
+                  <div className="grey-text text-darken-3 note-date">
                     { moment(notification.time.toDate()).fromNow() }
                   </div>
                 </li>
