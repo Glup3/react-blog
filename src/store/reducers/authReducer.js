6df433a42cf4ce0b02b91b010123ef5b210f5011
Glup3 <glupstar312@gmail.com>
2018-12-28 12:52:28 +0100

    add log out functionality

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -15,6 +15,9 @@ const authReducer = (state = initState, action) => {
         ...state,
         authError: null
       }
+
+    case 'SIGNOUT_SUCCESS':
+      return state;
     
     default:
       return state;
