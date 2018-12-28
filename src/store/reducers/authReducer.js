eaee5d5ae7bc5ec6dad969fe9544be51aa64a1d4
Glup3 <glupstar312@gmail.com>
2018-12-28 13:49:52 +0100

    add sign up functionality

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -18,6 +18,18 @@ const authReducer = (state = initState, action) => {
 
     case 'SIGNOUT_SUCCESS':
       return state;
+
+    case 'SIGNUP_SUCCESS':
+      return {
+        ...state,
+        authError: null
+      }
+    
+    case 'SIGNUP_ERROR':
+      return {
+        ...state,
+        authError: action.err.message
+      }
     
     default:
       return state;
